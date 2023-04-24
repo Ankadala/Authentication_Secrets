6bc9ad237607176f5bc08f70bd872085f4c68cea
Jeevan <jeevan.ankadala@gmail.com>
2023-04-25 02:41:12 +0530

    Add Environment Vars

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-
+require('dotenv').config();
 const express=require("express");
 const ejs=require("ejs");
 const bodyParser=require("body-parser");
@@ -19,9 +19,9 @@ const userSchema=new mongoose.Schema({
 	password:String
 });
 
-const secret="Thisisourlittlesecret";
-
-userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
+// const secret="Thisisourlittlesecret";
+console.log(process.env.API_KEY);
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 
 const User=mongoose.model("User",userSchema);
 
